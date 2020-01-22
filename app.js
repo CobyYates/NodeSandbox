58c9781fb56430f4f9ba0c3cfc9a2d734d9eb2c7
cobylyates <cobylyates@gmail.com>
2020-01-22 14:47:37 -0700

    fixed passing back html

--- app.js
+++ app.js
@@ -20,11 +20,23 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`<h2 style="text-align: center; padding-top: 5%;">Welcome to my api</h2>
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+  </head>
+  <body>
+      <h2 style="text-align: center; padding-top: 5%;">Welcome to my api</h2>
                 <div style="text-align: center;">
                     <img style="max-width: 900px;" src="https://images.pexels.com/photos/402028/pexels-photo-402028.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
                     <h3>visit my <a href="/api">api</a> page</h3>
-                </div>`);
+                </div>
+  </body>
+  </html>
+  `);
 });
 
 //export app for the tests
